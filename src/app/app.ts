4c76dcfb4ecd0d7d93b048e57aa1e9d44924468c
Leandro Nakamashi <leandro.nakamashi@gmail.com>
2025-07-16 17:41:18 +0900

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Main } from './main/main';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Main],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class App {
+  protected title = 'frontend';
+}
---
+++ src/app/main/main.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-main',
+  imports: [FormsModule],
+  templateUrl: './main.html',
+  styleUrl: './main.scss',
+})
+export class Main implements OnInit {
+  socket!: WebSocket;
+  mensagens: string[] = [];
+  mensagem: string = '';
+  totalClientes: number = 1;
+  constructor() {}
+
+  ngOnInit(): void {
+    // Initialization logic here
+    this.socket = new WebSocket('ws://localhost:3000');
+
+    this.socket.onmessage = (event) => {
+      this.mensagens.push('Recebida: ' + event.data);
+    };
+  }
+
+  enviar() {
+    if (this.mensagem.trim()) {
+      this.socket.send(this.mensagem);
+      this.mensagens.push('Você: ' + this.mensagem);
+      this.mensagem = '';
+    }
+  }
+}
