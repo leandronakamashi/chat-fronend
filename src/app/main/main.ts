4b1a048cd5d255e91df3644c108ed6d3cb25dfca
Leandro Nakamashi <leandro.nakamashi@gmail.com>
2025-07-22 04:01:59 +0900

    mudando url para railway

--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -20,10 +20,10 @@ export class Main implements OnInit {
 
   ngOnInit(): void {
     // Initialization logic here
-    this.socket = new WebSocket('ws://localhost:3000');
-    // this.socket = new WebSocket(
-    //   'wss://chat-backend-production-02f0.up.railway.app'
-    // );
+    // this.socket = new WebSocket('ws://localhost:3000');
+    this.socket = new WebSocket(
+      'wss://chat-backend-production-02f0.up.railway.app'
+    );
     this.route.queryParams.subscribe((params) => {
       this.nome = params['nome'] || '';
     });
